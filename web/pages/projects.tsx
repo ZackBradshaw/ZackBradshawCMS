7533887afd0bd7e426a2c641d75cb45fdcf2d3cf
zack <zackzbradshaw@gmail.com>
2022-11-30 10:21:33 -0600

    Created system to sort through posts and put them in the coresponding section

--- web/pages/projects.tsx
+++ web/pages/projects.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import Link from "next/link";
-import urlFor from "../utils/urlfor";
-import Card from "../components/Card";
 import Layout from "../components/Layout";
 import SectionHeader from "../components/SectionHeader";
 import styles from "../styles/projects.module.scss";
 import groq from "groq";
 import client from "../client";
+import { PostsList } from "../components/PostsList";
 
 const Projects = ({ posts }) => {
   // console.log("posts", posts);
+
   return (
     <Layout title="Projects | Zack.dev">
       <div className={styles.container}>
@@ -20,32 +19,17 @@ const Projects = ({ posts }) => {
           <p>List of my projects</p>
         </header>
         <div className={styles.section}>
-          <SectionHeader title="web-dev" />
           <div className={styles.projects}>
-            {posts.length > 0 &&
-              posts.map(
-                ({
-                  _id,
-                  title = "",
-                  slug = "",
-                  body = "",
-                  publishedAt = "",
-                  categories = "",
-                  mainImage = "",
-                  projectLink = "",
-                }: any) =>
-                  slug && (
-                    <div className={styles.projects} key={_id}>
-                      <Card
-                        categories={categories}
-                        title={title}
-                        img={urlFor(mainImage).url()}
-                        description={body}
-                        link={projectLink}
-                      />
-                    </div>
-                  )
-              )}
+            <SectionHeader title="web-dev" />
+            <PostsList posts={posts} tag="Web Dev" />
+          </div>
+          <div className={styles.projects}>
+            <SectionHeader title="game-dev" />
+            <PostsList posts={posts} tag="Game Dev" />
+          </div>
+          <div className={styles.projects}>
+            <SectionHeader title="art" />
+            <PostsList posts={posts} tag="Art" />
           </div>
         </div>
       </div>
