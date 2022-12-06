0f9984944a30416577e385838edd11a89cb58f25
zack <zackzbradshaw@gmail.com>
2022-12-05 18:29:05 -0600

    Added more socials

--- web/components/Contacts.tsx
+++ web/components/Contacts.tsx
@@ -22,10 +22,31 @@ const Contacts = () => {
             <img src="../assets/Discord.svg" alt="" />
             <h2>zackbradshaw#7647</h2>
           </Link>
-          <div className={styles.link}>
+          <Link
+            href="https://twitter.com/ZackBradshaw10"
+            className={styles.link}
+            target="_blank"
+          >
+            <img src="../assets/Twitter.svg" />
+            <h2>@ZackBradshaw10</h2>
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/zack-bradshaw-54898b18a/"
+            className={styles.link}
+            target="_blank"
+          >
+            <img src="../assets/Linkedin.svg" alt="" />
+            <h2>Zack Bradshaw</h2>
+          </Link>
+
+          <Link href="mailto:zack@zackbradshaw.com" className={styles.link}>
             <img src="../assets/Email.svg" alt="" />
-            <h2>Zackzbradshaw@gmail.com</h2>
-          </div>
+            <h2>zack@zackbradshaw.com</h2>
+          </Link>
+          <Link href="tel:4179558082" className={styles.link}>
+            <img src="../assets/326545_call_phone_icon.svg" alt="" width={25} />
+            <h2>417-955-8082</h2>
+          </Link>
         </div>
       </div>
     </div>
