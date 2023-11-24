7d3de01572835b32dc2e8c410d73f77bbf024c36
Zack <zack@zackbradshaw.com>
2023-11-23 21:30:26 -0800

    chore: debugging

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -1,9 +1,6 @@
 import { defineCliConfig } from '@sanity/cli'
 
 export default defineCliConfig({
-    project: {
-        basePath: '/studio'
-    },
     api: {
         "projectId": "vg1sjyep",
         "dataset": "production"
