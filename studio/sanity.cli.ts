1a47f0fc3052276ed66227eb7dc6f104cf2bf692
Zack <zack@zackbradshaw.com>
2023-11-23 20:03:36 -0800

    feat: deploy sanity

---
+++ studio/sanity.cli.ts
@@ -0,0 +1,11 @@
+import { defineCliConfig } from '@sanity/cli'
+
+export default defineCliConfig({
+    project: {
+        basePath: '/studio'
+    },
+    api: {
+        "projectId": "vg1sjyep",
+        "dataset": "production"
+    }
+})
