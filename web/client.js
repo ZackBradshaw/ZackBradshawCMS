88a9b0b4019c5d7fb95c924fa863fbb129e59ae7
Zack <zack@zackbradshaw.com>
2023-12-03 13:57:01 -0800

    fix: debug client

--- web/client.js
+++ web/client.js
@@ -1,8 +1,17 @@
 // client.js
 import sanityClient from "@sanity/client";
 
+sanityClient
+  .fetch('*[_type == "post"]')
+  .then(posts => {
+    console.log('All posts:', posts)
+  })
+  .catch(err => {
+    console.error('Error fetching posts:', err)
+  })
+  
 export default sanityClient({
   projectId: "vg1sjyep", // you can find this in sanity.json
   dataset: "production", // or the name you chose in step 1
-  useCdn: false, // `false` if you want to ensure fresh data
+  useCdn: true, // `false` if you want to ensure fresh data
 });
